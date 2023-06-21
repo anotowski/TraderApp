690834ef4123f3b4560aabb3e79138e0393401c6
anotowski <anotowski@gmail.com>
2023-06-21 21:21:12 +0200

    fixes + add localsettings json

--- TraderApp/TraderApp.Domain/Models/StockDetails/StockDetails.cs
+++ TraderApp/TraderApp.Domain/Models/StockDetails/StockDetails.cs
@@ -4,6 +4,6 @@ public class StockDetails
 {
     public int NumberOfComments { get; set; }
     public string Sentiment { get; set; }
-    public double SentimentScore { get; set; }
+    public double? SentimentScore { get; set; }
     public string Ticker { get; set; }
 }
--- TraderApp/TraderApp.Infrastructure/HttpClientAdapters/TradeSite/TradeSiteResponse.cs
+++ TraderApp/TraderApp.Infrastructure/HttpClientAdapters/TradeSite/TradeSiteResponse.cs
@@ -4,6 +4,6 @@ public class TradeSiteResponse
 {
     public int no_of_comments { get; set; }
     public string sentiment { get; set; }
-    public double sentiment_score { get; set; }
+    public double? sentiment_score { get; set; }
     public string ticker { get; set; }
 }
